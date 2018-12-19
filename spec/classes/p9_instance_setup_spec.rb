86cf6be8e47aa0a0efac7a9f1c7a1246209dfb8d
RandellP <randell@puppet.com>
2018-12-19 10:09:45 -0800

    first

---
+++ spec/classes/p9_instance_setup_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe 'p9_instance_setup' do
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) { os_facts }
+
+      it { is_expected.to compile }
+    end
+  end
+end
